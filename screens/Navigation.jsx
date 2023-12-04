6577581bb11ae806a286274ab97ca3ce2d060fad
harryviennot <harry.viennot@epitech.eu>
2023-12-04 20:51:42 +0100

    Add tab bar icons and update navigation structure

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -1,8 +1,10 @@
-// import { Image, Pressable, Text, View } from "react-native";
+import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { dark } from "../utils/colors";
 
 import LandingScreen from "./auth/LandingScreen";
 import EmailScreen from "./auth/EmailScreen";
@@ -17,21 +19,64 @@ const AuthStack = createNativeStackNavigator();
 const Authentification = () => {
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
-      <AuthStack.Screen name="Connect" component={EmailScreen} options={{ headerShown: false }}/>
-      <AuthStack.Screen name="Password" component={PasswordScreen} options={{ headerShown: false }}/>
+      <AuthStack.Screen
+        name="Landing"
+        component={LandingScreen}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="Connect"
+        component={EmailScreen}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="Password"
+        component={PasswordScreen}
+        options={{ headerShown: false }}
+      />
     </AuthStack.Navigator>
   );
-}
+};
 
 const MainStack = () => {
   return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Home" component={HomeScreen} />
+    <MainTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused
+              ? require("../assets/tabbarIcons/dashboard-filled.png")
+              : require("../assets/tabbarIcons/dashboard.png");
+          } else if (route.name === "Create") {
+            iconName = require("../assets/tabbarIcons/create.png");
+          } else if (route.name === "Settings") {
+            iconName = focused
+              ? require("../assets/tabbarIcons/settings-filled.png")
+              : require("../assets/tabbarIcons/settings.png");
+          }
+          return <Image style={{ width: 30, height: 30 }} source={iconName} />;
+        },
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 90,
+          borderTopWidth: 0.5,
+          backgroundColor: dark.primary,
+        },
+      })}
+    >
+      <MainTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
       <MainTab.Screen name="Settings" component={SettingsScreen} />
     </MainTab.Navigator>
   );
-}
+};
 
 const Navigation = () => {
   const { isLoggedIn } = useAuthContext();
@@ -39,14 +84,10 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-        {isLoggedIn ? (
-          <MainStack />
-        ) : (
-            <Authentification />
-        )}
+        {isLoggedIn ? <MainStack /> : <Authentification />}
       </SafeAreaProvider>
     </NavigationContainer>
   );
-}
+};
 
 export default Navigation;
