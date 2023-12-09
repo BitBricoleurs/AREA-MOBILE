e4d74005ab57a1636be0f8b77e517a3d31ef8b38
harryviennot <harry.viennot@epitech.eu>
2023-12-09 18:38:24 +0100

    Add Calendar component and update package.json

---
+++ components/trigger/calendar.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import DateTimePicker from "react-native-ui-datepicker";
+
+import MyText from "../../utils/myText";
+import { dark } from "../../utils/colors";
+import { useWorkflowContext } from "../../contexts/WorkflowContext";
+
+const Calendar = ({ update }) => {
+  const todaysDate = new Date();
+  const [date, setDate] = useState(todaysDate.toISOString().split("T")[0]);
+  const { trigger, setTrigger } = useWorkflowContext();
+
+  const onChange = (newDate) => {
+    setDate(newDate.split(" ")[0]);
+  };
+
+  useEffect(() => {
+    const newParams = {
+      ...trigger.params,
+      date: date,
+    };
+    setTrigger({
+      ...trigger,
+      params: newParams,
+    });
+  }, [date, update]);
+
+  return (
+    <View style={styles.container}>
+      <DateTimePicker
+        testID="dateTimePicker"
+        value={date}
+        mode={"date"}
+        display="default"
+        minimumDate={todaysDate - 1000 * 60 * 60 * 24}
+        onValueChange={onChange}
+        calendarTextStyle={{
+          color: dark.white,
+          fontFamily: "Outfit_500Medium",
+        }}
+        selectedTextStyle={{
+          color: dark.white,
+          fontFamily: "Outfit_500Medium",
+        }}
+        weekDaysTextStyle={{
+          color: dark.white,
+          fontFamily: "Outfit_500Medium",
+        }}
+        headerButtonStyle={{
+          color: dark.primary,
+        }}
+        headerButtonColor="white"
+        headerTextStyle={{
+          color: dark.white,
+          fontFamily: "Outfit_500Medium",
+        }}
+        monthContainerStyle={{
+          backgroundColor: dark.secondary,
+        }}
+        yearContainerStyle={{
+          backgroundColor: dark.secondary,
+        }}
+        selectedItemColor={dark.purple}
+      />
+    </View>
+  );
+};
+
+export default Calendar;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+    backgroundColor: dark.secondary,
+    borderRadius: 8,
+    padding: 16,
+  },
+  dateText: {
+    color: dark.white,
+    fontSize: 16,
+  },
+});
