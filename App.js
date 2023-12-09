2ad82d42c638cdf9c63e2795cf4cf7c69acf455e
harryviennot <harry.viennot@epitech.eu>
2023-12-09 14:39:55 +0100

    Remove unused imports and components

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import { useState, useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
 import { useFonts, Outfit_500Medium } from "@expo-google-fonts/outfit";
 import * as SplashScreen from "expo-splash-screen";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
-import MyText from "./utils/myText";
-import { dark } from "./utils/colors";
 import Navigation from "./screens/Navigation";
 import CustomSplashScreen from "./screens/splashscreen/CustomSplashScreen";
 
