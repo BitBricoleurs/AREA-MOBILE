23f52459dd836336c22bfa95e032b85a56d59a1b
harryviennot <harry.viennot@epitech.eu>
2023-12-20 10:38:48 +0100

    Refactor SettingsScreen component and add refresh button

--- screens/SettingsScreen.jsx
+++ screens/SettingsScreen.jsx
@@ -7,35 +7,33 @@ import { useAuthContext } from "../contexts/AuthContext";
 
 const SettingsScreen = () => {
   const [microsoftLink, setMicrosoftLink] = useState([]);
+  const [refresh, setRefresh] = useState(false); // TODO: use this to refresh the buttons
   const { dispatchAPI } = useAuthContext();
 
   const getLoginButtons = async () => {
-    const response = await dispatchAPI("GET", "/microsoft-login");
-    console.log(response);
-    if (typeof response === "string") {
-      const parsedResponse = JSON.parse(response);
-      setMicrosoftLink(parsedResponse.authorization_url);
-    } else {
-      // If the response is already an object
-      setMicrosoftLink(response.authorization_url);
-    }
+    const { data } = await dispatchAPI("GET", "/microsoft-login");
+    setMicrosoftLink(data.authorization_url);
   };
 
   useEffect(() => {
     getLoginButtons();
-  }, []);
+  }, [refresh]);
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        {microsoftLink && (
-          <Button
-            title="Connect to Microsoft"
-            onPress={() => {
-              Linking.openURL(microsoftLink);
-            }}
-          />
-        )}
+        <Button
+          title="Connect to Microsoft"
+          onPress={() => {
+            Linking.openURL(microsoftLink);
+          }}
+        />
+        <Button
+          title="Refresh"
+          onPress={() => {
+            setRefresh(!refresh);
+          }}
+        />
       </View>
       <View style={styles.logoutButtonContainer}>
         <Button title="Log out" onPress={() => dispatchAPI("LOGOUT")} />
